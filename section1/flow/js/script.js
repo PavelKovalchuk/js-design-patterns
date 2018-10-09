571c85c3fb62143e5b74d1864bbfee19fb8b8972
PavelKovalchuk <kovpavanat@gmail.com>
2018-10-09 21:40:43 +0300

    module DP

--- section1/flow/js/script.js
+++ section1/flow/js/script.js
@@ -1,43 +1,54 @@
-var o = {
-    leadself: 'Me: ',
-    leadcomputer: "PC: ",
-    aSaid: ["This is a Cyber Chat"],
-    msgYes: "Yes, that's a great idea.",
-    msgNo: "No, that must be a mistake.",
-    aSassyStuff: ["Like mold on books, grow myths on history.",
+var chatModule = (function () {
+
+    // Private scope
+    var leadself = 'Me: ',
+        leadcomputer = "PC: ",
+        aSaid = ["This is a Cyber Chat"],
+        msgYes = "Yes, that's a great idea.",
+        msgNo = "No, that must be a mistake.",
+        aSassyStuff = ["Like mold on books, grow myths on history.",
         "She moved like a poem and smiled like a sphinx.",
         "As long as we don’t die, this is gonna be one hell of a story.",
         "She laughed, and the desert sang.",
-        "You’ve got about as much charm as a dead slug."],
+        "You’ve got about as much charm as a dead slug."];
 
-    talk: function(message) {
-        this.echo(this.leadself + message);
-    },
+    function echo (message) {
+        aSaid.push('<div>' + message + '</div>');
 
-    replayYesNo: function() {
-        var msg = Math.random() > 0.5 ? this.msgYes : this.msgNo;
-        this.echo(this.leadcomputer + msg);
-    },
-
-    saySassyStuff: function () {
-        var msg = Math.random() > 0.5 ? this.msgYes : this.msgNo;
-        this.echo(this.leadcomputer + msg);
-    },
-
-    echo: function (message) {
-        this.aSaid.push('<div>' + message + '</div>');
-
-        var aSaidLength = this.aSaid.length,
+        var aSaidLength = aSaid.length,
             start = Math.max(aSaidLength - 6,0),
             out = "";
         for(var i = start; i < aSaidLength; i++){
-            out += this.aSaid[i];
+            out += aSaid[i];
         }
 
         $('.advert').html(out);
         $('#talk span').text(message);
     }
-};
+
+    return {
+        talk: function(message) {
+            echo(leadself + message);
+        },
+
+        replayYesNo: function() {
+            var msg = Math.random() > 0.5 ? msgYes : msgNo;
+            echo(leadcomputer + msg);
+        },
+
+        saySassyStuff: function () {
+            var msg = Math.random() > 0.5 ? msgYes : msgNo;
+            echo(leadcomputer + msg);
+        },
+    };
+})();
+
+$(doc).ready(function(){
+    chatModule.talk("this is great");
+    chatModule.replayYesNo();
+    chatModule.saySassyStuff();
+});
+
 
 
 
