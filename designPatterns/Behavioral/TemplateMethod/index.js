32b67385fb20037787842f77e681238afb9d947c
PavelKovalchuk <kovpavanat@gmail.com>
2018-10-30 22:55:17 +0200

    Template Method

---
+++ designPatterns/Behavioral/TemplateMethod/index.js
@@ -0,0 +1,83 @@
+/**
+ * Template Method
+ *
+ *  defines the skeleton of how certain algorithm could be performed
+ *  but defers the implementation of those steps to the children classes.
+ *
+ *   It lets subclass redefine certain steps of an algorithm without changing the algorithm's structure.
+ *
+ */
+
+// **************************** //
+
+//  base class that specifies the skeleton for the build algorithm
+
+class Builder {
+    // Template method
+    build() {
+        this.test();
+        this.lint();
+        this.assemble();
+        this.deploy();
+    }
+}
+
+// **************************** //
+
+// implementations
+
+class AndroidBuilder extends Builder {
+    test() {
+        console.log('Running android tests');
+    }
+
+    lint() {
+        console.log('Linting the android code');
+    }
+
+    assemble() {
+        console.log('Assembling the android build');
+    }
+
+    deploy() {
+        console.log('Deploying android build to server');
+    }
+}
+
+class IosBuilder extends Builder {
+    test() {
+        console.log('Running ios tests');
+    }
+
+    lint() {
+        console.log('Linting the ios code');
+    }
+
+    assemble() {
+        console.log('Assembling the ios build');
+    }
+
+    deploy() {
+        console.log('Deploying ios build to server');
+    }
+}
+
+// **************************** //
+
+const androidBuilder = new AndroidBuilder();
+androidBuilder.build();
+
+// Output:
+// Running android tests
+// Linting the android code
+// Assembling the android build
+// Deploying android build to server
+
+const iosBuilder = new IosBuilder();
+iosBuilder.build();
+
+// Output:
+// Running ios tests
+// Linting the ios code
+// Assembling the ios build
+// Deploying ios build to server
